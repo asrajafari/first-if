41068413010c186f332fae5f72ea2f30a650bdc0
asrajafari <168227642+asrajafari@users.noreply.github.com>
2024-09-10 12:12:16 +0330

    commit all

--- firs if/Program.cs
+++ firs if/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int thisYear = int.Parse(Console.ReadLine()) ;
+int year = int.Parse (Console.ReadLine());
+
+
+if (year == thisYear)
+{
+    Console.WriteLine(year+"is this year");
+}
+else if (year < thisYear)
+{
+    Console.WriteLine("we are in the past");
+}
+else
+{
+    Console.WriteLine("we are in future");
+}
